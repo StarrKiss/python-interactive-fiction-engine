c9232ee43252fd21e353443e79ed821f92a6a687
Gregory Starr <gregorystarr00@mgail.com>
2019-08-22 02:07:44 -0500

    Fixed repeat error

--- textblock.py
+++ textblock.py
@@ -33,15 +33,12 @@ class Command:
 		self.obj.call()
 
 class Store:
-	text1 = Section('assets/nulltest', [])
-	text2 = Section('assets/nulltest', [])
-	text5 = Section('assets/nulltest', [])
 
 
-	def __init__(self):
-		self.text1 = Section('assets/text1', [Command('text2', "West")])
-		self.text5 = Section('assets/text5', [])
-		self.text2 = Section('assets/text2', [Command('text5', "Investigate"), Command('text1', "Go Back")])
+	
+	text1 = Section('assets/text1', [Command('text2', "West")])
+	text5 = Section('assets/text5', [])
+	text2 = Section('assets/text2', [Command('text5', "Investigate"), Command('text1', "Go Back")])
 
 	def getItem(self, item):
 		return self.__dict__.get(item)
